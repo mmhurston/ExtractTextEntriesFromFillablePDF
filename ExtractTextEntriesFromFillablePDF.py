879aed3f75b2abcc8b4595e7b9a05ae73ad2d77b
Meredith Hurston <55521427+mmhurston@users.noreply.github.com>
2022-06-02 23:46:31 -0400

    Initial commit

---
+++ ExtractTextEntriesFromFillablePDF.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+pip install PyPDF2
+
+
+# In[5]:
+
+
+import pandas as pd
+import PyPDF2 as pypdf
+
+
+# In[33]:
+
+
+pdfobject=open(r'C:\Users\mhursto1\Desktop\FY23Indicators\TM_FY23 Quality Indicator Worksheet.pdf','rb') #replace file path and name as appropriate.
+pdf=pypdf.PdfFileReader(pdfobject)
+dictionary = pdf.getFormTextFields()
+dictionary
+# Create DataFrame from simple dictionary i.e dictionary with key and simple value like integer or string value.
+df = pd.DataFrame(list(dictionary.items()))
+#set display max so text doesn't truncate
+pd.set_option('display.max_colwidth', None)
+df
+
